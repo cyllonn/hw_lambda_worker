d8c5b4e5fc24d0ae9d859be228201138e2d74cfb
Pavel <cylonn@yandex.ru>
2023-04-15 14:00:49 +0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.function.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+        int a = calc.plus.apply(1 ,2);
+        int b = calc.minus.apply(1,1);
+        int c = calc.devide.apply(a , b); //при делении на ноль программа заканивается ошибкой
+        calc.println.accept(c);
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener errorListener = System.out::println;
+        Worker worker = new Worker(listener, errorListener);
+        worker.start(33);
+    }
+
+}
