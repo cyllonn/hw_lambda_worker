b41c929e4c3bf62bdba4a870a5ce2cd1828917d1
Pavel <cylonn@yandex.ru>
2023-04-15 14:02:43 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,7 @@
 import java.util.function.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Calculator calc = Calculator.instance.get();
-        int a = calc.plus.apply(1 ,2);
-        int b = calc.minus.apply(1,1);
-        int c = calc.devide.apply(a , b); //при делении на ноль программа заканивается ошибкой
-        calc.println.accept(c);
+    public static void main(String[] args) {       
         OnTaskDoneListener listener = System.out::println;
         OnTaskErrorListener errorListener = System.out::println;
         Worker worker = new Worker(listener, errorListener);
